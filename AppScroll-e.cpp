a64e6d529b5a2d9856fa7ce685eb9667ea310dd9
Giurgiu Razvan <razvan.giurgiu@tapptitude.com>
2023-05-25 21:31:49 +0300

    lab4

--- AppScroll-e.cpp
+++ AppScroll-e.cpp
@@ -40,15 +40,6 @@ void DisplWindow(HWND hWnd);
 //
 //--------------------------------------------------------------------
 
-// In the AppScroll-e.cpp source file, first call the PciBaseAddress() function to determine the base address of the PCIe extended configuration space and store the base address
-// in a global variable. If the function returns 0 or 1, the base address cannot be successfully
-// determined, and in this case the application should return with an error code. Otherwise, display the base address as two double-words. Next, write a function that returns a pointer to a
-// PCIe function’s configuration header using the PCIe enhanced configuration mechanism. The
-// function has as input parameters the bus number, device number, and PCIe function number,
-// and it returns a pointer to a PCI_CONFIG0 structure containing the PCIe function’s configuration header. The enhanced configuration mechanism is described in Section 2.6.3. In this
-// function, use the global variable containing the base address of the PCIe extended configuration space. Finally, use this function to search for PCIe devices on each bus between 0 and 63,
-// for each device (0..31), and for each function (0..7) of a device. For each existing PCIe device, the following information should be displayed (on separate lines):
-
 PPCI_CONFIG0 returnConfigHeader(BYTE busNumber, BYTE deviceNumber, BYTE pci_eFunctionNumber){
 
 
@@ -58,14 +49,6 @@ PPCI_CONFIG0 returnConfigHeader(BYTE busNumber, BYTE deviceNumber, BYTE pci_eFun
 	return (PPCI_CONFIG0)qwFuncAddr;
 }
 
-
-DWORD readWord(BYTE busNumber, BYTE deviceNumber, BYTE pci_eFunctionNumber, DWORD number){
-
-	DWORD dwFuncAddr = dwFuncAddr | (busNumber << 16) | (deviceNumber << 11) | (pci_eFunctionNumber << 8) | (number << 2) | (1 << 31);
-	__outpdw(PCI_CONFIG_ADR, dwFuncAddr);
-	return  __inpdw(PCI_CONFIG_DATA);
-
-}
 
 int AppScroll(HWND hWnd)
 {
@@ -92,25 +75,15 @@ int AppScroll(HWND hWnd)
 	cLine += 2;
 	DisplWindow(hWnd);
 
-// Extend Application 2.7.2 to display additional information about the existing
-// PCIe devices in the computer. The additional information that should be displayed is the following:
-// • Vendor ID, vendor descriptor;
-// • Device ID, chip descriptors.
-// Use the PCI-vendor-dev.h header file that has been added to the project. To display
-// the vendor descriptor, search in the PciVenTable array using the vendor ID as search key
-// and display the CONST CHAR *VenFull member of the PCI_VENTABLE structure. To display
-// the chip descriptors, search in the PciDevTable array using the vendor ID and device ID as
-// search keys and display the CONST CHAR *Chip and CONST CHAR *ChipDesc members of
-// the PCI_DEVTABLE structure.
-// Notes
-// • The number of entries in the PciVenTable array is defined as PCI_V
+//--------------------------------------------------------------------
+// To be completed with the application's code
+//--------------------------------------------------------------------
 
 
 	for (int busNumber = 0; busNumber < 16; busNumber++){
 		for (int deviceNumber = 0; deviceNumber < 32; deviceNumber++){
 			for (int functionNumber = 0; functionNumber < 8; functionNumber++){
 				PPCI_CONFIG0 current = returnConfigHeader(busNumber, deviceNumber, functionNumber);
-				
 				WORD wVendorID = _inmw((DWORD_PTR)&current->VendorID);
 					if (wVendorID != 0xFFFF){
 
@@ -121,7 +94,6 @@ int AppScroll(HWND hWnd)
 						WORD vendorId = _inmw((DWORD_PTR)&current->SubSystID);
 
 						wsprintf(szBuffer[cLine++], "Bus Number: %d, Device Number: %d, Function Number: %d", busNumber, deviceNumber, functionNumber);
-						wsprintf(szBuffer[cLine++], "VendorID: %d",wVendorID);
 						wsprintf(szBuffer[cLine++], "ClassCode: %x, Sub Class Code: %x", baseClass, subClass);
 						wsprintf(szBuffer[cLine++], "Programming Interface : %x, Subsystem vendorID : %x, SubsystemID : %x", progInterface, subsystemVendorId, vendorId);
 						PCI_CLASS_TABLE *tab = PciClassTable;
@@ -148,20 +120,8 @@ int AppScroll(HWND hWnd)
 						wsprintf(szBuffer[cLine++], "----------------------------------------------------------------------------------------------------------------------------------");
 					}
 			}
-			}
 		}
-	
-	
-	
-		WORD VendorID = LOWORD(readWord(0, 31, 3, 0));
-		if (VendorID != 0xFFFF){
-			BYTE base = HIWORD(readWord(0, 31, 3, 2)) | HIBYTE(LOWORD(readWord(0, 31, 3, 2)));
-			wsprintf(szBuffer[cLine++], "BaseClass : %d", base);
-			wsprintf(szBuffer[cLine++], "VendorID : %d", VendorID);
-		}
-
-
-
+	}
 
 
 
